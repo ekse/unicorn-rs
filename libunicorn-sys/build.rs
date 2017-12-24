4c9fc6e249001eb355d5397a9b852ef0ddc3bbab
Malte Thiesen <malte@kamalook.de>
2017-12-24 01:01:49 +0100

    Windows projects can be build without the "Developer Command Prompt"

--- libunicorn-sys/build.rs
+++ libunicorn-sys/build.rs
@@ -3,11 +3,38 @@ extern crate gcc;
 extern crate os_type;
 extern crate pkg_config;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::env;
+use std::ffi::{OsString, OsStr};
 
 use build_helper::rustc::{link_search, link_lib};
+
+fn get_vsdevcmd_path() -> OsString {
+    let vswhere_output = Command::new(r"build_tools\vswhere.exe")
+        .args(&[
+            "-latest",
+            "-products",
+            "*",
+            "-requires",
+            "Microsoft.VisualStudio.Component.VC.Tools.x86.x64",
+            "-property",
+            "installationPath"])
+        .output()
+        .expect("failed to execute vswhere.exe");
+
+    if !vswhere_output.status.success() {
+        fail("vswhere failed to locate MSVC");
+    }
+
+    let vswhere_stdout = String::from_utf8(vswhere_output.stdout)
+        .expect("vswhere output is not valid UTF-8");
+    
+    [&vswhere_stdout.trim(), "Common7", "Tools", "VsDevCmd.bat"]
+        .iter()
+        .collect::<PathBuf>()
+        .into_os_string()
+}
 
 fn main() {
     if !build_helper::windows() {
@@ -30,19 +57,20 @@ fn main() {
     };
 
     if build_helper::target::triple().env().unwrap_or("").contains("msvc") {
-        let libpath = env::var("LIB").unwrap();
-        env::set_var("LIB", libpath + ";" + &out_dir);
-        
+        let build_cmd_path: PathBuf = [
+            env::current_dir().expect("failed to retrieve current directory").as_path(),
+            Path::new("build_tools"),
+            Path::new("msvc_build.bat")].iter().collect();
+
         let platform_toolset = match env::var("PLATFORMTOOLSET") {
             Ok(x) => format!("PlatformToolset={};", x),
             Err(_) => "".to_owned(),
         };
 
-        let properties = "/p:OutDir=".to_owned() + &out_dir + "/;" + &platform_toolset
-                         + "useenv=true;Configuration=Release";
-        let status = match Command::new("msbuild")
-                            .args(&["msvc/unicorn.sln", "/t:unicorn_static", &properties])
-                            .current_dir("unicorn").status() {
+        let status = match Command::new(build_cmd_path)
+            .args(&[&get_vsdevcmd_path(), OsStr::new(&out_dir), OsStr::new(&platform_toolset)])
+            .current_dir("unicorn")
+            .status() {
             Ok(status) => status,
             Err(e) => fail(&format!("failed to execute command: {}", e)),
         };
---
+++ libunicorn-sys/build_tools/msvc_build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set curdir=%cd%
+
+call "%~1" -arch=x64 -host_arch=x86 -no_logo
+if %errorlevel% neq 0 (
+    exit /b 1
+)
+
+cd %curdir%
+
+set LIB=%LIB%;"%~2"
+
+msbuild msvc/unicorn.sln /t:unicorn_static /p:OutDir="%~2/";%3useenv=true;Configuration=Release
